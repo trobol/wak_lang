02f849341c37bd05db7dcf6c03df12e1f4d03b1d
Thornton Fernbacher <thorfernbacher@gmail.com>
2021-07-22 13:20:09 -0400

    add token parsing

--- src/wak_lang/tail_parser.c
+++ src/wak_lang/tail_parser.c
@@ -32,27 +32,34 @@ typedef struct {
 } wak_parse_state;
 
 
-#define PARSER_PARAMS wak_parse_state* state, Token* tok 
-#define PARSER_ARGS state, tok
-#define PARSER_RET Token*
+#define PTC_PARAMS wak_parse_state* state, Token* tok 
+#define PTC_ARGS state, tok
+#define PTC_RET Token*
 
-#define PARSER_ERR_PARAMS PARSER_PARAMS, unsigned int line
-#define PARSER_ERR_ARGS PARSER_ARGS, __LINE__
-
-
-inline PARSER_RET ptc_append(PARSER_PARAMS, IR_Statement statement);
-PARSER_RET ptc_append_dispatch(PARSER_PARAMS, IR_Statement statement);
+#define PTC_ERR_PARAMS PTC_PARAMS, unsigned int line
+#define PTC_ERR_ARGS PTC_ARGS, __LINE__
 
 
-PARSER_RET ptc_dispatch(PARSER_PARAMS);
+inline PTC_RET ptc_append(PTC_PARAMS, IR_Statement statement);
+PTC_RET ptc_append_dispatch(PTC_PARAMS, IR_Statement statement);
 
 
-PARSER_RET ptc_token(PARSER_PARAMS);
-PARSER_RET ptc_identifier(PARSER_PARAMS);
+PTC_RET ptc_dispatch(PTC_PARAMS);
 
-void ptc_err_report(PARSER_ERR_PARAMS, const char* message); 
 
-PARSER_RET ptc_err_fatal(PARSER_ERR_PARAMS, const char* message);
+PTC_RET ptc_token(PTC_PARAMS);
+PTC_RET ptc_identifier(PTC_PARAMS);
+
+PTC_RET ptc_func_decl(PTC_PARAMS);
+PTC_RET ptc_type_decl(PTC_PARAMS);
+PTC_RET ptc_end_block(PTC_PARAMS);
+PTC_RET ptc_return(PTC_PARAMS);
+PTC_RET ptc_eof(PTC_PARAMS);
+PTC_RET ptc_break(PTC_PARAMS);
+
+void ptc_err_report(PTC_ERR_PARAMS, const char* message); 
+
+PTC_RET ptc_err_fatal(PTC_ERR_PARAMS, const char* message);
 
 
 Parsed_Module parse(Token_Module module) {
@@ -76,7 +83,7 @@ Parsed_Module parse(Token_Module module) {
 
 // call this for each iteration
 // begining and end of loop
-PARSER_RET ptc_dispatch(PARSER_PARAMS) {
+PTC_RET ptc_dispatch(PTC_PARAMS) {
 
 	if (WAK_UNLIKELY(tok >= state->limit || tok == 0)) {
 		return tok;
@@ -85,36 +92,76 @@ PARSER_RET ptc_dispatch(PARSER_PARAMS) {
 	switch (tok->type)
 	{
 	case TOKEN_TYPE_TOKEN:
-		return ptc_token(PARSER_ARGS);
+		return ptc_token(PTC_ARGS);
 	case TOKEN_TYPE_IDENTIFIER:
-		return ptc_identifier(PARSER_ARGS);
+		return ptc_identifier(PTC_ARGS);
 	default:
-		return ptc_err_fatal(PARSER_ERR_ARGS, "unexpected literal `%s`");
+		return ptc_err_fatal(PTC_ERR_ARGS, "unexpected literal `%s`");
 	}
 }
 
 
-
-
-
-PARSER_RET ptc_token(PARSER_PARAMS) {
-	return ptc_err_fatal(PARSER_ERR_ARGS, "token parsing not implemented");
+PTC_RET ptc_token(PTC_PARAMS) {
+	wak_assert(tok->type == TOKEN_TYPE_TOKEN);
+	
+	switch (tok->token)
+	{
+		case TOKEN_KEYWORD_FUNC:
+			return ptc_func_decl(PTC_ARGS);
+		case TOKEN_KEYWORD_TYPE:
+			return ptc_type_decl(PTC_ARGS);
+		case '}':
+			return ptc_end_block(PTC_ARGS);
+		case TOKEN_KEYWORD_RETURN:
+			return ptc_return(PTC_ARGS);
+		case TOKEN_EOF:
+			return ptc_eof(PTC_ARGS);
+		case TOKEN_BREAK:
+			return ptc_break(PTC_ARGS);
+		default:
+			return ptc_err_fatal(PTC_ERR_ARGS, "unknown token");
+	}
 }
 
-PARSER_RET ptc_identifier(PARSER_PARAMS) {
-	return ptc_err_fatal(PARSER_ERR_ARGS, "identifier parsing not implemented");
+PTC_RET ptc_break(PTC_PARAMS) {
+	tok++;
+	return ptc_dispatch(PTC_ARGS);
+}
+
+PTC_RET ptc_func_decl(PTC_PARAMS) {
+	return ptc_err_fatal(PTC_ERR_ARGS, "unimplemented");
+}
+
+PTC_RET ptc_type_decl(PTC_PARAMS) {
+	return ptc_err_fatal(PTC_ERR_ARGS, "unimplemented");
+}
+
+PTC_RET ptc_end_block(PTC_PARAMS) {
+	return ptc_err_fatal(PTC_ERR_ARGS, "unimplemented");	
+}
+
+PTC_RET ptc_return(PTC_PARAMS) {
+	return ptc_err_fatal(PTC_ERR_ARGS, "unimplemented");
+}
+
+PTC_RET ptc_eof(PTC_PARAMS) {
+	return ptc_err_fatal(PTC_ERR_ARGS, "unimplemented");
 }
 
 
+PTC_RET ptc_identifier(PTC_PARAMS) {
+	return ptc_err_fatal(PTC_ERR_ARGS, "identifier parsing not implemented");
+}
 
-PARSER_RET ptc_err_fatal(PARSER_ERR_PARAMS, const char* msg) {
+
+PTC_RET ptc_err_fatal(PTC_ERR_PARAMS, const char* msg) {
 	ptc_err_report(state, tok, line, msg);
 	printf("\nparser reported a fatal syntax error\n");
 	return 0;
 }
 
 
-void ptc_err_report(PARSER_ERR_PARAMS, const char* msg) {
+void ptc_err_report(PTC_ERR_PARAMS, const char* msg) {
 	
 	Token_Pos* positions = state->token_module.positions->start;
 	size_t tok_index = tok - state->start;
