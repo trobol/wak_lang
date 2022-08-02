bb695fac4c909b2ddefd08603167ecd43a62c404
Thornton Fernbacher <thorfernbacher@gmail.com>
2022-08-01 20:12:41 -0700

    add mbps

--- tests/lex_speed.c
+++ tests/lex_speed.c
@@ -6,6 +6,9 @@
 #include <wak_lang/parser.h>
 #include <float.h>
 
+#include <errno.h>
+
+#include <coz.h>
 
 int main(int argc, char *argv[]) {
 
@@ -19,14 +22,17 @@ int main(int argc, char *argv[]) {
 	double min = DBL_MAX;
 	double max = 0;
 	double first_time = 0;
-	long double total = 0;
+	double total = 0;
 
-	const char* filepath = "../long_example_file.txt";
+
+
+	const char* filepath = "./long_example_file.txt";
 
 
 	file_descriptor src_fd = fd_open(filepath, 0);
 	if (src_fd == -1) {
 		printf("error: could not open file\n");
+		printf("Error description is : %s\n",strerror(errno));
 		return 1;
 	}
 	mmapped_file src_mmf = fd_mem_map(src_fd);
@@ -50,14 +56,21 @@ int main(int argc, char *argv[]) {
 		printf("%d: %fms\n", i, time);
 	}
 
-	double average = total / (long double)iterations;
+	double average = total / (double)iterations;
+
+	double size_kb = src_mmf.length / 1000.0;
+	double min_mbps = size_kb / min;
+	double max_mbps = size_kb / max;
+	double average_mbps = size_kb / average;
+	double first_mbps = size_kb / first_time;
+
 	printf("\nRESULTS:\n");
 	printf("iterations: %d\n", iterations);
-	printf("total: %Lfms\n", total);
-	printf("min: %fms\n", min);
-	printf("max: %fms\n", max);
-	printf("average: %fms\n", average);
-	printf("first: %fms\n", first_time);
+	printf("total:   %9.4fms\n", total);
+	printf("min: 	 %9.4fms (%4.1fmbps)\n", min, min_mbps );
+	printf("max:     %9.4fms (%4.1fmbps)\n", max, max_mbps);
+	printf("average: %9.4fms (%4.1fmbps)\n", average, average_mbps);
+	printf("first:   %9.4fms (%4.1fmbps)\n", first_time, first_mbps);
 
 	// free file resources
 	fd_mem_unmap(&src_mmf);
